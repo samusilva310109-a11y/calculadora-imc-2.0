62f763a9ea065f21a940d410548d59b7c063fe29
samusilva310109-a11y <samusilva310109@gmail.com>
2025-09-29 13:57:24 -0300

    Calculadora corrigida

--- src/br/senai/sp/jandira/ds1t/calculadoraimc/model/Imc.java
+++ src/br/senai/sp/jandira/ds1t/calculadoraimc/model/Imc.java
@@ -18,23 +18,45 @@ public class Imc {
         System.out.print("Insira o peso do paciente(Kg): ");
         pesoDoPaciente = leitor.nextDouble();
 
-        System.out.print("Insira a altura do paciente(cm): ");
+        System.out.print("Insira a altura do paciente(m): ");
         alturaDoPaciente = leitor.nextDouble();
 
         leitor.close();
         System.out.println("-----------------------------");
+
+        calcularImc();
     }
     public void calcularImc(){
         double alturaAoQuadrado = alturaDoPaciente * alturaDoPaciente;
         resultadoImc = pesoDoPaciente / alturaAoQuadrado;
 
+        definirSituacaoDoPaciente();
+
     }
     public void definirSituacaoDoPaciente(){
         if (resultadoImc < 18.5){
             situacaoCorporal = "abaixo do peso";
-        } else if (resultadoImc >=18.5) {
-            
+        } else if (resultadoImc >=18.5 && resultadoImc <= 24.9) {
+            situacaoCorporal = "peso ideal (parabéns)";
+        } else if (resultadoImc >= 25.0 && resultadoImc <= 29.9) {
+            situacaoCorporal = "levemente acima do peso";
+        } else if (resultadoImc >= 30.0 && resultadoImc <= 34.9) {
+            situacaoCorporal = "obesidade grau I";
+        } else if (resultadoImc >= 35.0 && resultadoImc <= 39.9) {
+            situacaoCorporal = "obesidade grau II";
+        }else{
+            situacaoCorporal = "obesidade grau III";
         }
+
+        exibirResultados();
+    }
+    public void exibirResultados(){
+        System.out.println("---RESULTADO---");
+        String resultadoFormatado = String.format ("%.2f", resultadoImc);
+        System.out.println("Nome do aluno: " + nomeDoAluno);
+        System.out.println("Imc calculado: " + resultadoFormatado);
+        System.out.println("Status: " + situacaoCorporal);
+        System.out.println("-----------------------------");
     }
 
 
